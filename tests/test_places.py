a4fa63efdd4a8ffeb42a356d7d12a2e9065f2cb6
Fidele K <itfidele@gmail.com>
2023-11-17 19:50:02 +0300

    Add test for validating the number of provinces

---
+++ tests/test_places.py
@@ -0,0 +1,13 @@
+import pytest
+from rwanda.local import Places
+
+NUMBER_OF_PROVINCES = 5
+
+@pytest.fixture
+def validator():
+    return Places()
+
+
+
+def test_provinces(validator):
+    assert len(validator.provinces()) == 5
