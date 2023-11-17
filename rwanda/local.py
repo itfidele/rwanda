713c772d2af7f4eb5dda223b4da07fb93171c232
Fidele K <itfidele@gmail.com>
2023-11-17 19:49:22 +0300

    Refactor class name and add new methods for retrieving data

--- rwanda/local.py
+++ rwanda/local.py
@@ -1,12 +1,12 @@
 import json
 import os
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-my_file = os.path.join(THIS_FOLDER, 'locations.json')
+my_file = os.path.join(f'{THIS_FOLDER}/locations', 'locations.json')
 
 
-class Rwanda:
+class Places:
 
-    def __init__(self, province_name=None):
+    def __init__(self, province_name:str=None):
         with open(my_file, 'r') as fp:
             data = json.load(fp)
         self.datas = data
@@ -14,7 +14,7 @@ class Rwanda:
                                 for province in self.datas['provinces']]
         
 
-    def provinces(self):
+    def provinces(self)->list[str]:
         return self.provinces_names
     
     def districts(self,province_name):
@@ -22,5 +22,53 @@ class Rwanda:
 
         Args:
             province_name ([string]): [province name]
+        """
+        pass
+
+    def sectors(self,district_name):    
+        """Returns all sectors from a district
+
+        Args:
+            district_name ([string]): [district name]
+        """
+        pass
+
+    def cells(self,sector_name):
+        """Returns all cells from a sector
+
+        Args:
+            sector_name ([string]): [sector name]
+        """
+        pass
+
+    def villages(self,cell_name):
+        """Returns all villages from a cell
+
+        Args:
+            cell_name ([string]): [cell name]
+        """
+        pass
+
+    def get_province(self,province_name):
+        """Returns a province
+
+        Args:
+            province_name ([string]): [province name]
+        """
+        pass
+
+    def get_district(self,district_name):
+        """Returns a district
+
+        Args:
+            district_name ([string]): [district name]
+        """
+        return district_name
+
+    def get_sector(self,sector_name):
+        """Returns a sector
+
+        Args:
+            sector_name ([string]): [sector name]
         """
         pass
